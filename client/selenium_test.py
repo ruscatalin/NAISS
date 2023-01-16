0d62f607bc28185b5cb312cf9e1b105a15946504
ruscatalin <ruscatalin97@gmail.com>
2023-01-16 13:17:51 +0100

    testing and result generation are quite done

--- client/selenium_test.py
+++ client/selenium_test.py
@@ -1,75 +1,124 @@
 import os
 import pytest
 
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome, ChromeOptions, Firefox, FirefoxOptions, Safari, Ie, IeOptions
 from selenium.webdriver.common.by import By
 from seleniumwire import webdriver # pip install selenium-wire
+# the seleniumwire.cert needs to be manually to Truested Root Cert Authorities added to each browser in order to avoid certificate errors with external links
+
 
 import time
 import pandas as pd
 
 HOSTNAME = "localhost"
 PORT = 8899
-ADDRESS = "http://{}:{}/".format(HOSTNAME, PORT)
+
 WEBSITES = [
-    "websites/clean/evilsig_clean_external_jpg_index.html",
-    "websites/clean/evilsig_clean_external_png_index.html",
     "websites/clean/evilsig_clean_internal_jpg_index.html",
     "websites/clean/evilsig_clean_internal_png_index.html",
-    "websites/clean/nosig_clean_external_jpg_index.html",
-    "websites/clean/nosig_clean_external_png_index.html",
+    "websites/clean/evilsig_clean_external_jpg_index.html",
+    "websites/clean/evilsig_clean_external_png_index.html",
     "websites/clean/nosig_clean_internal_jpg_index.html",
     "websites/clean/nosig_clean_internal_png_index.html",
-    "websites/clean/sig_clean_external_jpg_index.html",
-    "websites/clean/sig_clean_external_png_index.html",
+    "websites/clean/nosig_clean_external_jpg_index.html",
+    "websites/clean/nosig_clean_external_png_index.html",
     "websites/clean/sig_clean_internal_jpg_index.html",
     "websites/clean/sig_clean_internal_png_index.html",
+    "websites/clean/sig_clean_external_jpg_index.html",
+    "websites/clean/sig_clean_external_png_index.html",
 
-    "websites/stego/evilsig_stego_external_jpg_index.html",
-    "websites/stego/evilsig_stego_external_png_index.html",
     "websites/stego/evilsig_stego_internal_jpg_index.html",
     "websites/stego/evilsig_stego_internal_png_index.html",
-    "websites/stego/nosig_stego_external_jpg_index.html",
-    "websites/stego/nosig_stego_external_png_index.html",
+    "websites/stego/evilsig_stego_external_jpg_index.html",
+    "websites/stego/evilsig_stego_external_png_index.html",
     "websites/stego/nosig_stego_internal_jpg_index.html",
     "websites/stego/nosig_stego_internal_png_index.html",
-    "websites/stego/sig_stego_external_jpg_index.html",
-    "websites/stego/sig_stego_external_png_index.html",
+    "websites/stego/nosig_stego_external_jpg_index.html",
+    "websites/stego/nosig_stego_external_png_index.html",
     "websites/stego/sig_stego_internal_jpg_index.html",
     "websites/stego/sig_stego_internal_png_index.html",
+    "websites/stego/sig_stego_external_jpg_index.html",
+    "websites/stego/sig_stego_external_png_index.html",
 ]
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-driver =  webdriver.Chrome(chrome_options=options)
+def get_driver(which_one):
+    selenium_options = {
+        'ca_cert': 'client/seleniumwire.crt'
+    }
+    match which_one:
+        case "chrome":
+            options = webdriver.ChromeOptions()
+            options.add_argument('--headless')
+            options.add_argument('--ignore-certificate-errors')
+            options.add_argument('--accept-insecure-certs')
+            return webdriver.Chrome(chrome_options=options, seleniumwire_options=selenium_options)
+        case "firefox":
+            options = webdriver.FirefoxOptions()
+            options.add_argument('--headless')
+            return webdriver.Firefox(options=options)
+        case "safari":
+            selenium_options['port'] = 4204
+            return webdriver.Safari(seleniumwire_options=selenium_options)
+        case "edge":
+            options = webdriver.EdgeOptions()
+            options.add_argument('--headless')
+            return webdriver.Edge(options=options, seleniumwire_options=selenium_options)
+
+# options = webdriver.ChromeOptions()
+# options.add_argument('--headless')
+# driver =  webdriver.Chrome(chrome_options=options)
 website_names = {website: website.split("/")[2:][0].split(".")[0] for website in WEBSITES}
-access_times = {website_names[website] : -1 for website in WEBSITES}
-unfiltered_percentages = {website_names[website] : -1 for website in WEBSITES}
+driver = get_driver("chrome")
+driver.close()
 
 
-def test_website_through_filter(website):
+def test_website(website):
+    global PORT
+    ADDRESS = "http://{}:{}/".format(HOSTNAME, PORT)
     # driver =  webdriver.Chrome(chrome_options=options)
     website_name = website_names[website]
     # unfiltered_percentage = 100
+    payload_size = 0
     timer = time.time()
     driver.get("{}{}".format(ADDRESS, website))
+
+    website_requests = []
+    is_from_website = False
+    for request in driver.requests:
+        if "{}{}".format(ADDRESS, website) == request.url:
+            is_from_website = True
+        if is_from_website:
+            website_requests.append(request)
     
     if "internal" in website: # these are the internally-loading websites
         requests = {200: [], 301: [], 400: [], 404: [], 420: [], 500: []}
         img_requests = 0
-        for request in driver.requests[-11:]: # only look at the last 11 elements
-            # only check for local images
+        
+        for request in website_requests:
+            # only count the image requests for 'internal' websites
             if request.url.endswith(".png") or request.url.endswith(".jpg") or request.url.endswith(".ico"):
                 img_requests += 1
+
+            if  request.response:
+                if request.response.status_code == 200 or request.response.status_code == 301 or request.response.status_code == 304:
+                    if not (request.url.endswith(".css") or request.url.endswith(".js")  or "fontawesome" in request.url):
+                        payload_size += len(request.response.body)
+                                    
                 request_response = request.response.status_code
                 requests[request_response].append(request.url)
 
         for response_code in requests.keys():
             if response_code == 420:
-                unfiltered_percentage = (1 - (len(requests[420]) / img_requests)) * 100
+                if len(requests[420]) == 0:
+                    unfiltered_percentage = 100
+                else:
+                    unfiltered_percentage = (1 - (len(requests[420]) / img_requests)) * 100
                 # unfiltered_percentages[website_name] = unfiltered_percentage
                 # print("{} : {:.2f}%".format(website.split("/")[2:][0], unfiltered_percentage))
+
+        # if len(requests[200]) == 1:
+        #     print(driver.requests[-10:])
                 
     elif "external" in website: # these are the externally-loading websites
         # check if the images are visible on the website
@@ -84,36 +133,70 @@ def test_website_through_filter(website):
         nr_of_external_images = 10 # it's usually 11, but the favicon is local, so we set it to 10
         unfiltered_percentage = (len(all_imgs) / nr_of_external_images) * 100
 
+        for request in website_requests:
+            if request.response:
+                response_code = request.response.status_code
+                if  response_code == 200  or response_code == 301 or response_code == 304:
+                    if not (request.url.endswith(".css") or request.url.endswith(".js") or "fontawesome" in request.url):
+                        payload_size += len(request.response.body)
         # print("{} : {:.2f}%".format(website_name, len(all_imgs)/nr_of_external_images*100))
+
+        # if payload_size == 0:
+        #     print(driver.requests[-10:])
     
     access_times[website_name] = time.time() - timer
     unfiltered_percentages[website_name] = unfiltered_percentage
-    # print("%: {} - time: {}".format(unfiltered_percentage, access_times[website_name]))
-    print("\u2713", website_name)
-    # driver.close()
+    payload_sizes[website_name] = payload_size / 1000 # in kilobytes
+    print("\u2713", website_name, payload_size/1000, "kB", access_times[website_name], "s")
 
 
-def test_websites():
+def test_websites(browser):
+    global unfiltered_percentages, access_times, payload_sizes, driver, PORT
+    # PORT = 8899
+    # access_times = {website_names[website] : -1 for website in WEBSITES}
+    # unfiltered_percentages = {website_names[website] : -1 for website in WEBSITES}
+    # payload_sizes = {website_names[website] : 0 for website in WEBSITES}
+    # driver = get_driver(browser)
+    # excel_row_index = 0
+    # for website in WEBSITES:
+    #     test_website(website)
+    #     website_name = website_names[website]
+
+    #     # save the access times and unfiltered percentages to a xlsx file
+    #     # the website name, access time and unfiltered percentage all go in the same row
+    #     excel = pd.read_excel("client/test_results/filtered_{}_test_results.xlsx".format(browser))
+    #     new_row = [website_name, unfiltered_percentages[website_name], access_times[website_name], payload_sizes[website_name]]
+    #     excel.loc[excel_row_index] = new_row
+    #     excel_row_index += 1
+
+    #     excel.to_excel("client/test_results/filtered_{}_test_results.xlsx".format(browser), index=False)
+
+    # print("\u2713", "filtered_{} excel written\n".format(browser))
+    # driver.close() # close the driver
+    PORT = 7777
+    access_times = {website_names[website] : -1 for website in WEBSITES}
+    unfiltered_percentages = {website_names[website] : -1 for website in WEBSITES}
+    payload_sizes = {website_names[website] : 0 for website in WEBSITES}
+
+    excel_row_index = 0
+    driver =  get_driver(browser)
     for website in WEBSITES:
-        test_website_through_filter(website)
-    
-    excel_row_index = 1
-    for website in WEBSITES:
+        test_website(website)
         website_name = website_names[website]
-
-        # save the access times and unfiltered percentages to a xls file
-        # the website name, access time and unfiltered percentage all go in the same row
-        # use pandas to write to the excel file, in the appropriate column
-        excel = pd.read_excel("client/test_results.xlsx")
-        # pd.concat([excel, pd.DataFrame([[website_name, access_times[website_name], unfiltered_percentages[website_name]]], columns=["website name", "access time", "unfiltered percentage"])])
-        new_row = [website_name, unfiltered_percentages[website_name], access_times[website_name]]
+        excel = pd.read_excel("client/test_results/unfiltered_{}_test_results.xlsx".format(browser))
+        new_row = [website_name, unfiltered_percentages[website_name], access_times[website_name], payload_sizes[website_name]]
         excel.loc[excel_row_index] = new_row
         excel_row_index += 1
-        excel.to_excel("client/test_results.xlsx", index=False)
+        excel.to_excel("client/test_results/unfiltered_{}_test_results.xlsx".format(browser), index=False)
+        # driver.delete_all_cookies()
 
-
+    driver.quit()
+    print("\u2713", "unfiltered_{} excel written\n".format(browser))
     
-test_websites()
-print("\u2713", "excel written")
+        
+# browsers = ['chrome', 'firefox', 'safari', 'edge']
+browsers = ['chrome', 'firefox', 'edge']
+for browser in browsers:
+    test_websites(browser)
 
     
--- client/visualise.py
+++ client/visualise.py
@@ -7,13 +7,30 @@ import pandas as pd
 import numpy as np
 
 
-excel = pd.read_excel("client/test_results.xlsx")
+f_chrome_excel = pd.read_excel("client/test_results/filtered_chrome_test_results.xlsx")
+f_firefox_excel = pd.read_excel("client/test_results/filtered_firefox_test_results.xlsx")
+f_edge_excel = pd.read_excel("client/test_results/filtered_edge_test_results.xlsx")
+# f_safari_excel = pd.read_excel("client/test_results/filtered_safari_test_results.xlsx")
+uf_chrome_excel = pd.read_excel("client/test_results/unfiltered_chrome_test_results.xlsx")
+uf_firefox_excel = pd.read_excel("client/test_results/unfiltered_firefox_test_results.xlsx")
+uf_edge_excel = pd.read_excel("client/test_results/unfiltered_edge_test_results.xlsx")
+# uf_safari_excel = pd.read_excel("client/test_results/unfiltered_safari_test_results.xlsx")
+
+excels = {
+    "chrome": [f_chrome_excel, uf_chrome_excel],
+    "firefox": [f_firefox_excel, uf_firefox_excel],
+    "edge": [f_edge_excel, uf_edge_excel],
+    # "safari": [f_safari_excel, uf_safari_excel],
+    "all": [f_chrome_excel, f_firefox_excel, f_edge_excel, uf_chrome_excel, uf_firefox_excel, uf_edge_excel]
+}
 
 categories = {
     "has_signature": ['sig', 'nosig', 'evilsig'],
     "has_stegoimages": ['clean', 'stego'],
     "image_type": ['external', 'internal'],
-    "image_format": ['jpg', 'png']
+    "image_format": ['jpg', 'png'],
+    "browser": ['chrome', 'firefox', 'safari', 'edge'],
+    "is_filtered": ['filtered', 'unfiltered']
 }
 
 all_categories = []
@@ -26,68 +43,97 @@ def plot_unfiltered_percentage_by_signature():
     # aggregate sig websites and compare them against aggregated nosig + evilsig websites
     sig_bin = []
     nosig_evilsig_bin = []
-    for website in excel['website name']:
-        if 'nosig' in website or 'evilsig' in website:
-            nosig_evilsig_bin.append(website)
-        else:
-            sig_bin.append(website)
+    for excel in excels['all']:
+        for website in excel['website name']:
+            # find the unfiltered percentage of the website
+            uf_percentage = excel.iloc[excel.index[excel['website name'] == website].tolist()[0]]['unfiltered percentage']
+            if 'nosig' in website or 'evilsig' in website:
+                nosig_evilsig_bin.append(uf_percentage)
+            else:
+                sig_bin.append(uf_percentage)
 
-    sig_unfiltered_percentage = excel.loc[excel['website name'].isin(sig_bin)]['unfiltered percentage'].mean()
-    nosig_evilsig_unfiltered_percentage = excel.loc[excel['website name'].isin(nosig_evilsig_bin)]['unfiltered percentage'].mean()
+    sig_unfiltered_percentage = np.array(sig_bin, dtype=object).mean()
+    nosig_evilsig_unfiltered_percentage = np.array(nosig_evilsig_bin, dtype=object).mean()
 
     with plt.style.context('bmh'):
-        plt.figure(figsize=(10, 8))
+        plt.figure(figsize=(15, 8))
         plt.bar(1, sig_unfiltered_percentage, color='g')
         plt.bar(2, nosig_evilsig_unfiltered_percentage, color='r')
         plt.xticks([1, 2], ['signed', 'no signature + malicious signature'])
-        plt.yticks([0, 1, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+        plt.yticks([0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
         plt.ylabel('Unfiltered percentage')
         plt.title('Unfiltered percentage for signed vs no signature + malicious signature websites')
-        plt.show()
+        # plt.show()
+        plt.savefig('client/test_results/unfiltered_percentage_by_signature.png')
         plt.close()
 
-def plot_access_time_by_category():
+def plot_measurement_by_category(measurement):
     bins = {category : [] for category in all_categories}
-    for website in excel['website name']:
-        access_time = excel.loc[excel['website name'] == website]['access time']
+    for browser in excels.keys():
+        if browser == 'all':
+            continue
+        for excel in excels[browser]:
+            for website in excel['website name']:
+                measure = excel.iloc[excel.index[excel['website name'] == website].tolist()[0]][measurement]
+                bins[browser].append(measure)
 
-        if 'nosig' in website:
-            bins['nosig'].append(access_time)
-        elif 'evilsig' in website:
-            bins['evilsig'].append(access_time)
-        elif 'sig' in website:
-            bins['sig'].append(access_time)
+                if excel is excels[browser][0]:
+                    bins['filtered'].append(measure)
+                else:
+                    bins['unfiltered'].append(measure)
 
-        if 'clean' in website:
-            bins['clean'].append(access_time)
-        elif 'stego' in website:
-            bins['stego'].append(access_time)
+                if 'nosig' in website:
+                    bins['nosig'].append(measure)
+                elif 'evilsig' in website:
+                    bins['evilsig'].append(measure)
+                elif 'sig' in website:
+                    bins['sig'].append(measure)
 
-        if 'external' in website:
-            bins['external'].append(access_time)
-        elif 'internal' in website:
-            bins['internal'].append(access_time)
+                if 'clean' in website:
+                    bins['clean'].append(measure)
+                elif 'stego' in website:
+                    bins['stego'].append(measure)
 
-        if 'jpg' in website:
-            bins['jpg'].append(access_time)
-        elif 'png' in website:
-            bins['png'].append(access_time)
+                if 'external' in website:
+                    bins['external'].append(measure)
+                elif 'internal' in website:
+                    bins['internal'].append(measure)
+
+                if 'jpg' in website:
+                    bins['jpg'].append(measure)
+                elif 'png' in website:
+                    bins['png'].append(measure)
+
+            
 
     with plt.style.context('bmh'):
-        plt.figure(figsize=(10, 5))
+        plt.figure(figsize=(20, 8))
         index = 1
+        max_avg = 0
         for category in bins:
+            if category == 'all':
+                continue
             value_to_plot = np.array(bins[category], dtype=object).mean()
+            if value_to_plot > max_avg:
+                max_avg = value_to_plot
             # compute a gradient color based on the value to plot
-            gradient_color = mcolors.Normalize(vmin=0, vmax=3.3, clip=True)(value_to_plot)
+            vmin, vmax = 0, 3
+            if measurement == 'size':
+                vmin, vmax = -50, 150
+            gradient_color = mcolors.Normalize(vmin=vmin, vmax=vmax, clip=True)(value_to_plot)
             gradient_color = plt.cm.Reds(gradient_color)
             plt.bar(index, value_to_plot, color=gradient_color)
             index += 1
+        plt.vlines([3.5, 5.5, 7.5, 9.5, 13.5], ymin=0, ymax=1.15*max_avg, color='k', linestyles='dotted', linewidth=1)
         plt.xticks(range(1, len(bins) + 1), bins.keys())
-        plt.ylabel('Average access time')
-        plt.title('Average access time for each category through NAISS')
-        plt.show()
+        plt.ylabel('Average {} ({})'.format(measurement, 'kB' if measurement == 'size' else 's'))
+        plt.title('Average {} for each category'.format(measurement))
+        # plt.show()
+        plt.savefig('client/test_results/{}_by_category.png'.format(measurement.replace(' ', '_')))
         plt.close()
 
+
+
 plot_unfiltered_percentage_by_signature()
-# plot_access_time_by_category()
+plot_measurement_by_category('access time')
+plot_measurement_by_category('size')
