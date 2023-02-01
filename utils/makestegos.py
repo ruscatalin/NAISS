03e3c437bc8c07a0f0a44ea700c51dadfa52699a
ruscatalin <ruscatalin97@gmail.com>
2023-02-01 11:01:06 +0100

    prep for stego making

--- utils/makestegos.py
+++ utils/makestegos.py
@@ -3,16 +3,18 @@ import os
 
 steganogan = SteganoGAN.load(architecture='dense')
 
-# get all images in the folder and encode them with the message "RENAISSANCE"
-# the output will be save in the stegoimages folder and will have stego adde to the begining of its name
+# the output will be save in the stegoimages folder and will have stego added to the begining of its name
 
-PATH_TO_IMAGES = "../server/website/images/"
+PATH_TO_IMAGES = "server/website/images/"
 PATH_TO_STEGOIMAGES = "{}stegoimages/".format(PATH_TO_IMAGES)
+
+# read the payload from utils/magecartsample.js
+PAYLOAD = open("utils/magecartsample.js", "r").read()
 
 for filename in os.listdir(PATH_TO_IMAGES):
     if filename.endswith((".png", ".jpg", ".jpeg", ".gif", ".ico", ".bmp")):
         print(PATH_TO_IMAGES+filename)
-        steganogan.encode(PATH_TO_IMAGES+filename, "{}stego_{}".format(PATH_TO_STEGOIMAGES, filename), "RENAISSANCE")
+        steganogan.encode(PATH_TO_IMAGES+filename, "{}stego_{}".format(PATH_TO_STEGOIMAGES, filename), PAYLOAD)
 
 print("Done encoding stegoimages!")
 print("Now we check the decoding of those images ...")
